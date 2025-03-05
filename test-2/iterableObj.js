8b5875aa84d03c4ee7c6812b4335539785c1e70d
Dandelion-F <2604746157@qq.com>
2025-03-05 17:26:05 +0800

    chore: additional submit

--- test-2/iterableObj.js
+++ test-2/iterableObj.js
@@ -1,9 +1,37 @@
-function run() {
+function run_1() {
     Object.prototype[Symbol.iterator] = function* () {
-        yield* Object.values(this)
+        yield* Object.values(this);
     };
 }
 
-run();
+function run_2() {
+    Object.prototype[Symbol.iterator] = function* () {
+        for (const key in this) {
+            if (Object.hasOwn(this, key)) {
+                yield this[key];
+            }
+        }
+    };
+}
+
+function run_3() {
+    Object.prototype[Symbol.iterator] = function () {
+        const values = Object.values(this);
+        let index = 0;
+        return {
+            next: () => {
+                if (index < values.length) {
+                    return {
+                        value: values[index++],
+                        done: false,
+                    };
+                }
+                return { done: true };
+            },
+        };
+    };
+}
+
+run_3();
 const [a, b] = { a: 1, b: 2 };
 console.log(a, b);
